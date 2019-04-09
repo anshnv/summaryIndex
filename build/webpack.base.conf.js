2d4473c5955567305289fd9d74f0970aab6cef3d
anshnv <anshnv@gmail.com>
2019-04-09 14:11:39 +0300

    asert:''

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -7,8 +7,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const PATHS = {
   src: path.join(__dirname, '../src'),
   dist: path.join(__dirname, '../dist'),
-  assets: './'
+  //assets: './'
   //попробовать assets:''
+  assets:''
 }
 
 
